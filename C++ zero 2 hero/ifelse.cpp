7d84b511b8acd0d51dfb93cc2c5cbfce34310bef
devanggg05 <giridevang5@gmail.com>
2024-06-10 12:53:03 +0530

    update

---
+++ C++ zero 2 hero/ifelse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a;
+    // cin>> a;
+    // cout<< "value of a: " << a << endl;
+
+    int x,y;
+    cin>>x>>y;
+    cout<< "value of x: " << x << "\n"<< "value of y: " << y << endl;
+    if (x>y){   
+        cout<<"x is grater than y"<< endl;
+    }
+    else{
+        cout<< "y is greater than x"<< endl;
+    }
+}
